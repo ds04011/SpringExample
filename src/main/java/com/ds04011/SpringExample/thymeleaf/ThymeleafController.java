baa9ac91bc186bc890537a5f05e0377b67082e95
ds04011 <tommyhw97@gmail.com>
2025-05-28 18:46:18 +0900

    ds04011 -타임리프 문법 실습

--- src/main/java/com/ds04011/SpringExample/thymeleaf/ThymeleafController.java
+++ src/main/java/com/ds04011/SpringExample/thymeleaf/ThymeleafController.java
@@ -1,7 +1,12 @@
 package com.ds04011.SpringExample.thymeleaf;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -35,6 +40,71 @@ public class ThymeleafController {
 		
 		
 		return "thymeleaf/test02";
+	}
+	
+	@GetMapping("/thymeleaf/test03")
+	public String test03(Model model) {
+		
+		
+		List<Map<String, Object>> membership = new ArrayList<>();
+
+		Map<String, Object> member = new HashMap<>();
+		member.put("name", "손오공");
+		member.put("phoneNumber", "010-1234-5678");
+		member.put("grade", "VIP");
+		member.put("point", 12040);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "저팔계");
+		member.put("phoneNumber", "010-4321-1234");
+		member.put("grade", "BASIC");
+		member.put("point", 3420);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "사오정");
+		member.put("phoneNumber", "010-8888-1111");
+		member.put("grade", "GOLD");
+		member.put("point", 1530);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "삼장");
+		member.put("phoneNumber", "010-0000-1234");
+		member.put("grade", "GOLD");
+		member.put("point", 8450);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "우마왕");
+		member.put("phoneNumber", "010-1111-2222");
+		member.put("grade", "BASIC");
+		member.put("point", 420);
+		membership.add(member);
+		
+		
+		model.addAttribute("result", membership);
+		
+		
+		return "thymeleaf/test03";
+	}
+	
+	
+	@GetMapping("/thymeleaf/ex03")
+	public String ex03(Model model) {
+		
+		Date date = new Date();
+		LocalDate localDate = LocalDate.now();
+		LocalDateTime localDateTime = LocalDateTime.now();
+		
+		model.addAttribute("date", date);
+		model.addAttribute("localDate", localDate);
+		model.addAttribute("localDateTime", localDateTime);
+		
+		
+		
+		return "thymeleaf/ex03";
 	}
 
 }
